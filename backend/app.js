a9436e9ee94cda228fcdf54b3eb12b6feb6f0be3
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-12-11 16:56:02 +0600

    feat: implement restaurant management features with CRUD operations and authentication

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
+const restaurantRoutes = require("./routes/restaurantRoutes");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
-app.use('/api/v1/auth', authRoutes);
-app.use('api/v1/auth', authRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/restaurants", restaurantRoutes);
 module.exports = app;
