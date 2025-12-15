4a17c85cbce2993f82891203e29c4ea9625bf1d1
Suraia Akter Mim <msuraia55@gmail.com>
2025-12-15 22:02:02 +0600

    Delete frontend directory

--- frontend/src/pages/common/Contact.jsx
+++
@@ -1,9 +0,0 @@
-const Contact = () => {
-  return (
-    <div className="min-h-screen bg-secondary p-4">
-      <h1 className="text-4xl font-bold">Contact Us</h1>
-    </div>
-  );
-};
-
-export default Contact;
