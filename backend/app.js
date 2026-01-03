3cf91aa5b2988bfe7638156555cb85115f3e505c
Chatok Junior <md.sakib.hos3n@gmail.com>
2026-01-04 00:25:11 +0600

    fix: add 'x-guest-session-id' to allowed CORS headers

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ const corsOption = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-guest-session-id']
 }
 
 app.use(cors(corsOption));
